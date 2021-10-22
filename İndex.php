0c9ec4f0e5fce953a01b83b39a6a721507541d9b
enes yilmaz <62097156+enes-yilmaz02@users.noreply.github.com>
2021-10-23 00:18:04 +0300

    Websitesi

---
+++ İndex.php
@@ -0,0 +1,213 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/c20485228a.js" crossorigin="anonymous"></script>
+    <title>Anasayfa | Web Sitesi</title>
+    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="owl/owl.carousel.min.css">
+    <link rel="stylesheet" href="owl/owl.theme.default.min.css">
+    
+</head>
+<body>
+     <section id="menu">
+         <div id="logo"> Web Sitesi </div>
+             <nav>
+                 <a href=""><i class="fas fa-home ikon"></i>Anasayfa</a>
+                 <a href=""><i class="fas fa-info ikon"></i>Hakkımızda</a>
+                 <a href=""><i class="fas fa-university ikon"></i>Eğitimler</a>
+                 <a href=""><i class="fas fa-users ikon"></i>Ekip</a>
+                 <a href=""><i class="fas fa-map-pin ikons"></i>İletişim</a>
+             </nav>
+       
+
+
+     </section>
+
+
+
+     <section id="anasayfa">
+     <div id="black">
+        
+     </div>
+     <div id="icerik">
+        <h2>Web Sitesi</h2>
+        <hr width="300" align="left">
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi, inventore quia! Repellat ad fugiat eaque, illo recusandae enim dolores, deserunt, cumque odio beatae consequatur reprehenderit quia magni ducimus alias architecto.</p>
+      </div>
+     </section>
+
+
+
+     <section id="hakkimizda">
+      <h3>Hakkımızda</h3>
+      <div class="container">
+      <div id="sol">
+          <h5 id="h5sol">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime quisquam perferendis architecto consequuntur ex harum earum! Possimus sequi culpa eveniet vel reprehenderit molestiae, exercitationem ratione, tempore quisquam explicabo, quod expedita! </h5>
+      </div>
+      <div id="sag">
+          <span>L</span>
+          <p id="psag">orem ipsum dolor sit amet consectetur adipisicing elit. Ullam quas minima aliquid, provident adipisci facere rerum facilis recusandae culpa illum iusto sunt quia nam explicabo est possimus eos voluptas. Repellat.</p>
+      </div>
+      <img src="img/about.jpg" alt="" class="img-fluid mt100">
+      <p id="pson">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, tempore iste aliquid nostrum hic ut illo, impedit deleniti, ab distinctio harum ratione labore dolorem tenetur ipsa veniam a laborum voluptatum!</p>
+      </div>
+     </section>
+
+
+     
+     <section id="egitimler">
+         <div class="container">
+             <h3>Eğitimler</h3>
+             <div class="Fixede">
+
+             <div class="card item" data-merge="1.5">
+                 <img src="img/r2.jpg" alt="" class="img-fluid ">
+                 <h5 class="baslikcard">html5 ve css3</h5>
+                 <p class="cardp">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, hic?</p>
+             </div>
+             <div class="card item" data-merge="1.5">
+                <img src="img/r2.jpg" alt="" class="img-fluid ">
+                <h5 class="baslikcard">html5 ve css3</h5>
+                <p class="cardp">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, hic?</p>
+            </div>
+            <div class="card item" data-merge="1.5">
+                <img src="img/r2.jpg" alt="" class="img-fluid ">
+                <h5 class="baslikcard">html5 ve css3</h5>
+                <p class="cardp">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, hic?</p>
+            </div>
+            
+
+           
+             </div>
+
+         </div>
+
+
+     </section>
+
+     <section id="ekip">
+         <div class="container ">
+             <h3 id="ekiph3">Ekip</h3>
+
+            
+          
+            <div class="sutun-sol-sag">
+                <img src="img/ekip1.jpg" alt="" class="img-fluid oval">
+                <h4 class="ekipismi">Hakan çelik</h4>
+                <p class="ekipp">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, maxime.</p>
+                <div class="ekip-ikon">
+                    <a href="#"><i class="fab fa-facebook social"></i></a>
+                    <a href="#"><i class="fab fa-instagram social"></i></a>
+                    <a href="#"><i class="fab fa-google-plus-g social"></i></a>
+                </div>
+            </div>
+            <div class="sutun-sol-sag">
+                <img src="img/ekip2.jpg" alt="" class="img-fluid oval">
+                <h4 class="ekipismi">Hakan çelik</h4>
+                <p class="ekipp">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, maxime.</p>
+                <div class="ekip-ikon">
+                    <a href="#"><i class="fab fa-facebook social"></i></a>
+                    <a href="#"><i class="fab fa-instagram social"></i></a>
+                    <a href="#"><i class="fab fa-google-plus-g social"></i></a>
+                </div>
+            </div>
+            <div class="sutun-sol-sag">
+                <img src="img/ekip3.jpg" alt="" class="img-fluid oval">
+                <h4 class="ekipismi">Hakan çelik</h4>
+                <p class="ekipp">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, maxime.</p>
+                <div class="ekip-ikon">
+                    <a href="#"><i class="fab fa-facebook social"></i></a>
+                    <a href="#"><i class="fab fa-instagram social"></i></a>
+                    <a href="#"><i class="fab fa-google-plus-g social"></i></a>
+                </div>
+            </div>
+         </div>
+     </section>
+
+     <section id="iletisim">
+         <div class="container">
+             <h3 id="h3iletisim">İletişim</h3>
+
+             <form action="İndex.php" method="POST">
+             <div id="iletisimopak">
+                 <div id="formgroup">
+                     <div id="solform">
+                         <input type="text" name="isim" placeholder="Ad soyad" required class="formkontrol">
+                         <input type="text" name="tel" placeholder="Telefon Numarası" required class="formkontrol">
+                     </div>
+                     <div id="sagform">
+                        <input type="email" name="mail" placeholder="E-mail " required class="formkontrol">
+                        <input type="text" name="konu" placeholder="Konu Başlığı" required class="formkontrol">
+                     </div>
+                     <textarea name="mesaj" id="" cols="30" placeholder="Mesajınız..." required rows="10" class="formkontrol"></textarea>
+                     <input type="submit" value="Gönder">
+                 </div>
+                 <div id="adres">
+                     <h4 id="adresbaslik">Adres:</h4>
+                     <p class="adresp">mehmet akif ersoy mahallesi</p>
+                     <p class="adresp">akyıldız caddesi</p>
+                     <p class="adresp">oguzbey sok. no:23</p>
+                     <p class="adresp">0212 212 21 21</p>
+                     <p class="adresp">info@gmail.com</p>
+                 </div>
+             </div>
+            </form>
+             <footer>
+
+              <div id="copyrigth">2020 | Tüm hakları Saklıdır</div>
+              <div id="socialfooter">
+                <a href="#"><i class="fab fa-facebook social"></i></a>
+                <a href="#"><i class="fab fa-instagram social"></i></a>
+                <a href="#"><i class="fab fa-google-plus-g social"></i></a>
+              </div>
+              <a href="#menu"><i class="fas fa-angle-up" id="up"></i></a>
+
+             </footer>
+         </div>
+     </section>
+
+
+     <script src="https://code.jquery.com/jquery-3.6.0.slim.min.js"
+     integrity="sha256-u7e5khyithlIdTpu22PHhENmPcRdFiHRjhAuHcs05RI="
+     crossorigin="anonymous"></script>
+        
+        <script src="owl/script.js"></script>
+
+
+</body>
+</html>
+
+
+<?php
+
+
+include("baglanti.php");
+
+if(isset($_POST["isim"],$_POST["tel"],$_POST["mail"],$_POST["konu"],$_POST["mesaj"]))
+{
+    $adsoyad=$_POST["isim"];
+    $telefon=$_POST["tel"];
+    $mail=$_POST["mail"];
+    $konu=$_POST["konu"];
+    $mesaj=$_POST["mesaj"];
+
+    $ekle="INSERT INTO `iletisim`(`id`, `adsoyad`, `telefon`, `mail`, `konu`, `mesaj`) VALUES ('[]','[$adsoyad]','[$telefon]','[$mail]','[$konu]','[$mesaj]')";
+    
+    if($baglan->query($ekle)===TRUE){
+        echo "<script> alert ('başarılı')</script>";
+
+    }
+    else
+    {
+        echo "<script> alert ('başarısız')</script>";
+    }
+    
+
+
+
+
+}
+?>
