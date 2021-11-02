0933a7546866b1609714dd831a8ca40c6d8d897c
enes yilmaz <62097156+enes-yilmaz02@users.noreply.github.com>
2021-11-02 20:10:37 +0300

    Update İndex.php

--- İndex.php
+++ İndex.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://kit.fontawesome.com/c20485228a.js" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/7cc2d38101.js" crossorigin="anonymous"></script>  
     <title>Anasayfa | Web Sitesi</title>
     <link rel="stylesheet" href="index.css">
     <link rel="stylesheet" href="owl/owl.carousel.min.css">
